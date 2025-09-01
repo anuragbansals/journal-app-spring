a02cfe2ead83492ed3ed5b1753f76db79409fd86
anuragbansals <anuragbansal@Anurags-MacBook-Air.local>
2025-09-01 20:46:47 +0530

    rest wip

---
+++ src/main/java/com/journal/jounalApp/controller/JournalEntryControllerV2.java
@@ -0,0 +1,54 @@
+package com.journal.jounalApp.controller;
+
+import com.journal.jounalApp.entity.JournalEntry;
+import com.journal.jounalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return journalEntryService.getEntries();
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
+        return myEntry;
+    }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
+        return journalEntryService.findById(myId).orElse(null);
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public boolean deleteJournalEntryById(@PathVariable ObjectId myId){
+         journalEntryService.deleteById(myId);
+         return true;
+    }
+
+    @PutMapping("id/{id}")
+    public JournalEntry updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry){
+        JournalEntry old = journalEntryService.findById(id).orElse(null);
+
+        if(old!=null){
+            old.setTitle(newEntry.getTitle()!=null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent()!=null && !newEntry.getContent().equals("")? newEntry.getContent() : old.getContent());
+        }
+        journalEntryService.saveEntry(old);
+        return old;
+    }
+}
---
+++ src/main/java/com/journal/jounalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package com.journal.jounalApp.repository;
+
+
+import com.journal.jounalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
